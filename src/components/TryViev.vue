cf75a3bf75a2fd274063eeb9be1134415800fef2
Pawlozz <112007360+Pawlozz@users.noreply.github.com>
2023-03-11 19:16:57 +0100

    Update TryViev.vue

--- src/components/TryViev.vue
+++ src/components/TryViev.vue
@@ -142,6 +142,10 @@ export default {
   methods: {
     changeProgress(stage) {
       this.stage = stage
+
+      if (stage == 1) {
+        window.location.reload(true);
+      }
     },
 
     cropImg(input) {
