77c9daaeb1f42605efa4b584312f6ca18bd11205
kremobil <47462737+kremobil@users.noreply.github.com>
2023-03-27 19:11:04 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -38,7 +38,7 @@ export default {
   data() {
     return {
       showPopup: false,
-      message: 'Wciąż udoskonalamy nasz model by był jak najdokładniejszy, pamiętaj jednak że nie ma on obecnie dokładność 85% może on się więc czasem pomylić.'
+      message: 'Wciąż udoskonalamy nasz model by był jak najdokładniejszy, pamiętaj jednak że ma on obecnie dokładność 85% może on się więc czasem pomylić.'
     }
   },
   methods: {
