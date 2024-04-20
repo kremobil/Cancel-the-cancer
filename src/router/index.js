547d242622628022224dd649c93132552808b0f1
Your Name <you@example.com>
2024-04-20 21:55:57 +0200

    added demo version

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeViev from '../components/HomeViev.vue';
-// import TryViev from '../components/TryViev.vue';
-import WorkInProgressView from "@/components/WorkInProgressView.vue";
+import TryViev from '../components/TryViev.vue';
+// import WorkInProgressView from "@/components/WorkInProgressView.vue";
 import AboutView from '../components/AboutView.vue';
 
 const routes = [
 	{ path: '/', component: HomeViev },
-	{ path: '/try', component: WorkInProgressView },
+	{ path: '/try', component: TryViev },
 	{ path: '/about', component: AboutView },
 ];
 
