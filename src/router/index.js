e967d9af0353042e00c888b437b14cf0f00a842b
kremobil <47462737+kremobil@users.noreply.github.com>
2024-05-10 18:36:54 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeViev from '../components/HomeViev.vue';
-import TryViev from '../components/TryViev.vue';
+// import TryViev from '../components/TryViev.vue';
 import WorkInProgressView from "@/components/WorkInProgressView.vue";
 import AboutView from '../components/AboutView.vue';
 
